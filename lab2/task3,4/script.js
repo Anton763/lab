7c64452c67510686ebbca871ac369c9cb92ee74b
Anton763 <54988300+Anton763@users.noreply.github.com>
2019-10-18 14:25:49 +0300

    Add files via upload

--- lab2/task3,4/script.js
+++ lab2/task3,4/script.js
@@ -1,17 +1,17 @@
-var video = document.querySelectorAll("iframe");
-var btn = document.querySelectorAll("button");
-var comment = document.querySelectorAll(".comment-block");
-video[0].classList.add("active");
-comment[0].classList.add("active");
+var video = document.querySelectorAll('iframe');
+var btn = document.querySelectorAll('button');
+var comment = document.querySelectorAll('.comment-block');
+video[0].classList.add('active');
+comment[0].classList.add('active');
 var current = 0;
 
-function changevideo(nvideo) {
-  video[current].classList.remove("active");
-  comment[current].classList.remove("active");
-  var src = video[current].getAttribute("src");
-  video[current].setAttribute("src", src);
-  current = nvideo;
-  video[current].classList.add("active");
-  comment[current].classList.add("active");
+function changevideo(videoIndex) {
+  video[current].classList.remove('active');
+  comment[current].classList.remove('active');
+  var src = video[current].getAttribute('src');
+  video[current].setAttribute('src', src);
+  current = videoIndex;
+  video[current].classList.add('active');
+  comment[current].classList.add('active');
 }
 console.log(comment);
