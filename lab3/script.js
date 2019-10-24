40740337a15677da4fe65a4a44ff398d39a114e5
Anton763 <54988300+Anton763@users.noreply.github.com>
2019-10-24 21:10:52 +0300

    Add files via upload

--- lab3/script.js
+++ lab3/script.js
@@ -1,15 +1,13 @@
-var themebutton = document.getElementById('theme'),
+var themeButton = document.getElementById('theme'),
   color = document.getElementById('background-color'),
   checkImg = document.querySelectorAll('img.check');
 
 function theme() {
-  if (themebutton.checked) {
-    color.classList.toggle('night');
-    checkImg[0].className = 'check none';
-    checkImg[1].className = 'check';
-  } else {
+  if (themeButton.checked) {
     color.classList.toggle('night');
     checkImg[0].className = 'check';
+  } else {
+    color.classList.toggle('night');
     checkImg[1].className = 'check none';
   }
 }
