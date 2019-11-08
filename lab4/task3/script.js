33ca5714175f78d4d0470b0572c1b55b68cc89f7
Anton763 <lyzlovanton@gmail.com>
2019-11-08 15:28:53 +0400

    commit

--- lab4/task3/script.js
+++ lab4/task3/script.js
@@ -1,3 +1,55 @@
-document.addEventListener('keydown', function (e) {
-console.log(e.keyCode);
+var selectedItem = 0,
+  directory = document.querySelectorAll('.main .container'),
+  itemRigth = document.querySelectorAll('.rigth .container-item'),
+  selectedDirectory = 0,
+  selectedItemRigth = 0,
+  item = document.querySelectorAll('.left .container-item');
+
+function goToItem(elementItem) {
+  item[selectedItem].className = 'container-item';
+  selectedItem = (elementItem + item.length) % item.length;
+  item[selectedItem].className = 'container-item selected';
+}
+
+function goToItemRigth(elementItemRigth) {
+  itemRigth[selectedItemRigth].className = 'container-item';
+  selectedItemRigth = (elementItemRigth + itemRigth.length) % itemRigth.length;
+  itemRigth[selectedItemRigth].className = 'container-item selected';
+}
+
+function goToDirectory(elementDirectory) {
+  directory[selectedDirectory].className = 'container';
+  selectedDirectory = (elementDirectory + directory.length) % directory.length;
+  directory[selectedDirectory].className = 'container selected';
+}
+document.addEventListener('keydown', function (event) {
+  if (event.code === 'ArrowRight') {
+    goToDirectory(selectedDirectory + 1);
+  }
+});
+document.addEventListener('keydown', function (event) {
+  if (event.code === 'ArrowLeft') {
+    goToDirectory(selectedDirectory - 1);
+  }
+});
+document.addEventListener('keydown', function (event) {
+  if (event.code === 'ArrowDown' && selectedDirectory == 0) {
+    goToItem(selectedItem + 1);
+  }
+});
+document.addEventListener('keydown', function (event) {
+  if (event.code === 'ArrowUp' && selectedDirectory == 0) {
+    goToItem(selectedItem - 1);
+  }
+});
+
+document.addEventListener('keydown', function (event) {
+  if (event.code === 'ArrowDown' && selectedDirectory == 1) {
+    goToItemRigth(selectedItemRigth + 1);
+  }
+});
+document.addEventListener('keydown', function (event) {
+  if (event.code === 'ArrowUp' && selectedDirectory == 1) {
+    goToItemRigth(selectedItemRigth - 1);
+  }
 });
